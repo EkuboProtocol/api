83823ec168d5866077d0c094e5fa3616077e0ba7
Moody Salem <moody.salem@gmail.com>
2023-06-21 18:45:38 -0400

    generate an svg

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,17 @@ const NFT_METADATA_PATH = /^\/(\d+)$/;
 const NFT_IMAGE_PATH = /^\/(\d+)\/image.svg$/;
 
 
+function generateSvg(token_id: bigint) {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+      <g fill="none" stroke="black" stroke-width="2">
+        <path fill="green" d="M25 10 Q35 20, 35 30 Q35 35, 30 38 Q35 40, 32 42 Q35 44, 30 47 Q25 48, 20 47 Q17 44, 18 42 Q20 40, 25 38 Q20 35, 20 30 Q20 20, 30 10 z"/>
+        <circle fill="pink" cx="17" cy="20" r="3"/>
+        <circle fill="pink" cx="33" cy="20" r="3"/>
+        <path fill="pink" d="M20 32 Q25 35, 30 32 T40 32"/>
+      </g>
+    </svg>`;
+}
+
 export default {
     async fetch(
         request: Request,
@@ -64,7 +75,8 @@ export default {
                 }
             });
         } else if (NFT_IMAGE_PATH.test(path)) {
-            return new Response('', {
+            let [, id] = NFT_METADATA_PATH.exec(path)!
+            return new Response(generateSvg(BigInt(id)), {
                 status: 200,
                 headers: {
                     'content-type': 'image/svg+xml',
