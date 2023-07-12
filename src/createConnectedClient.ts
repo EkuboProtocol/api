754810ab297c3fa68c04fe114c7eab2bbb621c92
Moody Salem <moody.salem@gmail.com>
2023-07-12 13:11:10 -0400

    delete the extra stuff added by accident

--- src/createConnectedClient.ts
+++ src/createConnectedClient.ts
@@ -3,18 +3,19 @@ import { Client } from "pg";
 import { CERTIFICATE_AUTHORITIES } from "./certs";
 
 export async function createConnectedClient(env: Env): Promise<Client> {
+  const ca = env.PGCERT ? CERTIFICATE_AUTHORITIES[env.PGCERT] : null;
+
   const client = new Client({
     user: env.PGUSER,
     password: env.PGPASSWORD,
     host: env.PGHOST,
     port: Number(env.PGPORT),
     database: env.PGDATABASE,
-    ssl:
-      env.PGCERT && CERTIFICATE_AUTHORITIES[env.PGCERT]
-        ? {
-            ca: CERTIFICATE_AUTHORITIES[env.PGCERT],
-          }
-        : false,
+    ssl: ca
+      ? {
+          ca,
+        }
+      : false,
   });
   await client.connect();
   return client;
