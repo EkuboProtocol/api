58dae0a98e98aa1b154d539d296e19cc56ae2b20
Moody Salem <moody.salem@gmail.com>
2023-09-16 09:25:30 -0400

    improve the description

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,38 @@ import { parseId } from "./parseId";
 import { Env } from "./env";
 import { ExecutionContext } from "@cloudflare/workers-types";
 import { createQueries } from "./createQueries";
+import MAINNET_TOKENS from "./tokens/mainnet.json";
+import Decimal from "decimal.js-light";
+
+const TOKENS_BY_CHAIN_ID: {
+  [key in "0x534e5f474f45524c49" | "0x534e5f4d41494e"]?: typeof MAINNET_TOKENS;
+} = {
+  ["0x534e5f4d41494e"]: MAINNET_TOKENS,
+};
+
+function findToken(
+  chainId: "0x534e5f474f45524c49" | "0x534e5f4d41494e",
+  address: string | bigint
+) {
+  return (TOKENS_BY_CHAIN_ID[chainId] ?? [])?.find(
+    (x) => BigInt(x.l2_token_address) === BigInt(address)
+  );
+}
+
+Decimal.set({ precision: 39 });
+
+const BASE = new Decimal("1.000001");
+
+function formattedPrice(
+  tick: bigint,
+  numeratorDecimals: number,
+  denominatorDecimals: number
+): string {
+  return BASE.pow(tick.toString())
+    .mul(new Decimal(10).pow(denominatorDecimals - numeratorDecimals))
+    .toSignificantDigits(6)
+    .toString();
+}
 
 const { preflight, corsify } = createCors({
   maxAge: 86400,
@@ -21,6 +53,20 @@ function numericToHex(x: bigint | number | string) {
 }
 
 const ADDRESS_REGEX = /^0x[a-fA-F0-9]+$/;
+
+const U128 = new Decimal(2).pow(128);
+
+function feeToPercent(fee: string) {
+  return new Decimal(fee).div(U128).mul(100).toSignificantDigits(4).toString();
+}
+
+function tickSpacingToPercent(tick_spacing: string) {
+  return BASE.pow(tick_spacing)
+    .sub(1)
+    .mul(100)
+    .toSignificantDigits(4)
+    .toString();
+}
 
 router
   .get<IRequest, CF>("/overview", async ({}, env) => {
@@ -340,13 +386,70 @@ router
     ];
 
     const origin = new URL(url).origin;
+    const token0 = findToken(env.STARKNET_CHAIN_ID, positionMetadata.token0);
+    const token1 = findToken(env.STARKNET_CHAIN_ID, positionMetadata.token1);
 
-    const metadata: NFTMetadata = {
-      name: `Ekubo NFT #${id}`,
-      description: "An NFT that represents a liquidity position in Ekubo",
-      image: `${origin}/${id}/image.svg`,
-      attributes: attributesStored,
-    };
+    let metadata: NFTMetadata;
+    if (token0 && token1) {
+      const reversed = token0.sortOrder >= token1.sortOrder;
+      const [numerator, denominator, lowerPrice, upperPrice] = reversed
+        ? [
+            token0,
+            token1,
+            formattedPrice(
+              -BigInt(positionMetadata.upper_bound),
+              token0.decimals,
+              token1.decimals
+            ),
+            formattedPrice(
+              -BigInt(positionMetadata.lower_bound),
+              token0.decimals,
+              token1.decimals
+            ),
+          ]
+        : [
+            token1,
+            token0,
+            formattedPrice(
+              BigInt(positionMetadata.lower_bound),
+              token1.decimals,
+              token0.decimals
+            ),
+            formattedPrice(
+              BigInt(positionMetadata.upper_bound),
+              token1.decimals,
+              token0.decimals
+            ),
+          ];
+
+      metadata = {
+        name: `${numerator.symbol} / ${
+          denominator.symbol
+        } : ${lowerPrice} <> ${upperPrice} : ${feeToPercent(
+          positionMetadata.fee
+        )}% / ${tickSpacingToPercent(positionMetadata.tick_spacing)}%`,
+        description: `A liquidity position in Ekubo consisting of the ${
+          numerator.name
+        } and ${
+          denominator.name
+        } tokens, active between the prices of ${lowerPrice} ${
+          numerator.symbol
+        } / ${denominator.symbol} to ${upperPrice} ${
+          denominator.symbol
+        }. This position charges a ${feeToPercent(
+          positionMetadata.fee
+        )}% fee on swaps.`,
+        image: `${origin}/${id}/image.svg`,
+        attributes: attributesStored,
+      };
+    } else {
+      metadata = {
+        name: `Ekubo NFT #${id}`,
+        description: "An NFT that represents a liquidity position in Ekubo",
+        image: `${origin}/${id}/image.svg`,
+        attributes: attributesStored,
+      };
+    }
 
     return json(metadata, {
       headers: {
