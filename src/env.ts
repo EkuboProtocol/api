76e4774726ff47322d5bf3245f74506ecd7a01f4
Moody Salem <moody.salem@gmail.com>
2023-10-09 17:28:09 -0400

    use hyperdrive

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,7 @@
 export interface Env {
   STARKNET_CHAIN_ID: "0x534e5f474f45524c49" | "0x534e5f4d41494e";
 
-  PG_CONNECTION_STRING: string;
+  PG_CONNECTION_STRING?: string;
+
+  HYPERDRIVE?: Hyperdrive;
 }
