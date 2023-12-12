3bce8e9c9269660425e1dc22c79bfbd6f1d07937
Moody Salem <moody.salem@gmail.com>
2023-12-12 18:20:13 -0500

    flip the prices

--- src/index.ts
+++ src/index.ts
@@ -807,8 +807,8 @@ router
               : data.map((d) => ({
                   ...d,
                   vwap: 1 / d.vwap,
-                  max_price: 1 / d.max_price,
-                  min_price: 1 / d.min_price,
+                  max_price: 1 / d.min_price,
+                  min_price: 1 / d.max_price,
                 })),
         },
         {
