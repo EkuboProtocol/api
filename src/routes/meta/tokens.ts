8c359011f1ebdbfcc885d6cdd5a2e4cf9e0159cf
Moody Salem <moody.salem@gmail.com>
2024-03-26 07:56:57 -0400

    increase max length by 1

--- src/routes/meta/tokens.ts
+++ src/routes/meta/tokens.ts
@@ -107,7 +107,7 @@ export async function getAllTokens(
       const name = shortString.decodeShortString(row.name).trim();
       const symbol = shortString.decodeShortString(row.symbol).trim();
       const l2_token_address = num.toHex(row.address);
-      if (symbol.length > 8) return;
+      if (symbol.length > 9) return;
       if (
         ["eku", "ekubo", "kubo", "kub", "kube"].includes(symbol.toLowerCase())
       ) {
