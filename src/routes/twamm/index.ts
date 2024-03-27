ca25268e7ff7754e4597617bf067aad4ce64d099
indreams <cfminimal@gmail.com>
2024-03-27 13:33:15 -0400

    update params spec (#41)

--- src/routes/twamm/index.ts
+++ src/routes/twamm/index.ts
@@ -173,11 +173,11 @@ export class GetSplitTWAPOrderByDate extends EkuboAPIRoute {
 
 const SellParamsType = z
   .object({
-    sellToken: AddressType.openapi({
+    sell_token: AddressType.openapi({
       example:
         "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
     }),
-    buyToken: AddressType.openapi({
+    buy_token: AddressType.openapi({
       example:
         "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
     }),
