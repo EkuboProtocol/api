3e43f97f4b127c88c4c0cae06f4f501b89ee9109
Moody Salem <moody.salem@gmail.com>
2024-04-11 12:52:56 -0400

    Revert "Revert "improve the data size of the per pool liquidity query""
    This reverts commit e7451746e5640bc4c2c92f6b0b8f2363c30d9b5e.

--- src/queries.ts
+++ src/queries.ts
@@ -1348,14 +1348,15 @@ export class Queries {
   }): Promise<{ [key_hash: string]: Tick[] }> {
     const { rows } = await this.client.query<{
       pool_key_hash: string;
-      liquidity_delta: string;
-      tick: number;
+      ticks: { tick: number; net_liquidity_delta_diff: string }[];
     }>({
       text: `
-        SELECT pool_key_hash, tick, net_liquidity_delta_diff AS liquidity_delta
+        SELECT pool_key_hash,
+               JSONB_AGG(JSONB_BUILD_OBJECT('tick', tick, 'net_liquidity_delta_diff',
+                                            net_liquidity_delta_diff)) AS ticks
         FROM per_pool_per_tick_liquidity_materialized
         WHERE pool_key_hash = ANY ($1::NUMERIC[])
-        ORDER BY pool_key_hash, tick
+        GROUP BY pool_key_hash
       `,
       values: [poolKeyHashes],
     });
@@ -1370,10 +1371,12 @@ export class Queries {
     );
 
     rows.forEach((value) => {
-      map[value.pool_key_hash].push({
-        tick: value.tick,
-        liquidityDelta: BigInt(value.liquidity_delta),
-      });
+      map[value.pool_key_hash] = value.ticks
+        .sort(({ tick: t0 }, { tick: t1 }) => t0 - t1)
+        .map(({ tick, net_liquidity_delta_diff }) => ({
+          tick,
+          liquidityDelta: BigInt(net_liquidity_delta_diff),
+        }));
     });
 
     return map;
