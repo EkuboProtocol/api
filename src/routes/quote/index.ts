4ef1d88a7cef9006b8d4d64a82fb4df46978295a
Moody Salem <moody.salem@gmail.com>
2024-04-27 12:26:59 -0400

    Revert "Revert "some cleanup""
    This reverts commit 110011faf111b88a45cf048403f92d270307fd9e.

--- src/routes/quote/index.ts
+++ src/routes/quote/index.ts
@@ -185,7 +185,7 @@ export class GetQuote extends EkuboAPIRoute {
     const otherTokenPrice =
       (
         await queries.getVolumeWeightedPriceOverPeriod({
-          baseToken: BigInt(env.ETH_TOKEN_ADDRESS),
+          baseToken: ETH_TOKEN_ADDRESS,
           quoteToken: otherToken,
           minSwapCount: 0,
         })
