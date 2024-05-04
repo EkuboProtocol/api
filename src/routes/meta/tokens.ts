795d52849697e373c6f0094007bcb8a5befae28a
Moody Salem <moody.salem@gmail.com>
2024-05-04 13:20:29 -0400

    ban socks symbol

--- src/routes/meta/tokens.ts
+++ src/routes/meta/tokens.ts
@@ -76,7 +76,7 @@ export type TokenInfo = z.infer<typeof TokenType>;
 const SEPOLIA_CHAIN_ID = BigInt(constants.StarknetChainId.SN_SEPOLIA);
 const MAINNET_CHAIN_ID = BigInt(constants.StarknetChainId.SN_MAIN);
 
-const BANNED_TOKEN_SYMBOLS = ["eku", "ekubo", "kubo", "kub", "kube"];
+const BANNED_TOKEN_SYMBOLS = ["eku", "ekubo", "kubo", "kub", "kube", "socks"];
 const ALPHANUMERIC_REGEX = /^[\x00-\x7F]*$/;
 
 export async function getAllTokens(
