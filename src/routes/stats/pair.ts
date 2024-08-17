143a003cd3db037e93cf5a692ccab5afe3892b4b
Moody Salem <moody.salem@gmail.com>
2024-08-17 11:18:28 -0400

    Revert "try again"
    This reverts commit d428fd9115400e6cf3d03619d3fd56927b83174d.

--- src/routes/stats/pair.ts
+++ src/routes/stats/pair.ts
@@ -217,7 +217,7 @@ export class GetPairInfoPools extends EkuboAPIRoute {
       },
       {
         headers: {
-          "cache-control": "public, max-age=600",
+          "cache-control": "public, max-age=7200",
         },
       },
     );
