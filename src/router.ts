574684e8b59df0dbd7b2fab74b102fc3e49cc9e3
Moody Salem <moody.salem@gmail.com>
2025-12-08 13:16:15 -0500

    Revert "remove the duplicate nft route"
    This reverts commit 36ef8486abe364874cb71534bb4eaff123a59431.

--- src/router.ts
+++ src/router.ts
@@ -42,6 +42,7 @@ import {
   ListVotesOnProposal,
 } from "./routes/governance";
 import { ListLimitOrders } from "./routes/limit";
+import { GetNftImage, GetNftMetadata } from "./routes/nft";
 
 export const router = OpenAPIRouter({
   schema: {
@@ -90,6 +91,8 @@ export const router = OpenAPIRouter({
   .get(ListTwapOrders.route, ListTwapOrders)
   .get(ListLimitOrders.route, ListLimitOrders)
   .get(GetPositionNftImage.route, GetPositionNftImage)
+  .get(GetNftMetadata.route, GetNftMetadata)
+  .get(GetNftImage.route, GetNftImage)
   .get(ListProposals.route, ListProposals)
   .get(ListTopDelegates.route, ListTopDelegates)
   .get(ListVotesOnProposal.route, ListVotesOnProposal)
