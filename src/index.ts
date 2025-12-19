ce76cc36c8cd3fb0f5a27ec515cf4aa9b72783ae
Yohan Tancrez <yohantancrez@gmail.com>
2025-12-19 21:17:54 +0100

    add back cache

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ export default {
     // we do this outside of the router because we do not want to RE-CACHE a successful response by including the cache logic in the router handler
     if (cacheKey) {
       // const cached = await cache.match(cacheKey);
-      const cached = false;
+      const cached = await cache.match(cacheKey);
       if (cached) {
         const corsified = corsify(cached);
         corsified.headers.set("Access-Control-Allow-Origin", "*");
